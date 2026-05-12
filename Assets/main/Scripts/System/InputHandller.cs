868d45ae82b4d953592a77308652de39db459e52
hikidoharuki <hharu05@icloud.com>
2026-05-12 17:59:59 +0900

    hikido

--- Assets/main/Scripts/System/InputHandller.cs
+++ Assets/main/Scripts/System/InputHandller.cs
@@ -6,6 +6,7 @@ public class InputHandller : MonoBehaviour
     public Define _defineSO;
     public Transform leftTargetCircle;
     public Transform rightTargetCircle;
+    public NoteDate noteDate;
 
 
     private void Update()
@@ -22,6 +23,7 @@ public class InputHandller : MonoBehaviour
                 _defineSO.SetInput(touch.position.ReadValue());
             }
         }
+
         //キーボード
         // キーボード入力部分
         if (Keyboard.current.fKey.wasPressedThisFrame)
@@ -30,11 +32,27 @@ public class InputHandller : MonoBehaviour
             //判定円の「スクリーン座標」を逆算して送る
             Vector2 screenPos = Camera.main.WorldToScreenPoint(leftTargetCircle.position);
             _defineSO.SetInput(screenPos);
+
         }
         else if (Keyboard.current.jKey.wasPressedThisFrame)
         {
             Vector2 screenPos = Camera.main.WorldToScreenPoint(rightTargetCircle.position);
             _defineSO.SetInput(screenPos);
         }
+
+
+    }
+
+
+    //ショートノーツ時のプッシュ判定
+    //ノーツデータ内のノーツ種類と押しているかで判定を取る
+    private void SinglePush(bool isShortPush, NoteDate noteDate)
+    {
+       // if(Keyboard.current.fKey.)
+    }
+
+    private void LongPush(bool isLongPush, NoteDate noteDate) 
+    {
+
     }
 }
